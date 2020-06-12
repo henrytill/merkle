81a2ce367eee43bba2735345165e08626bdd641a
Henry Till <henrytill@gmail.com>
2020-06-11 23:05:31 -0400

    update for hlint >= 3.0

--- tests/HLint.hs
+++ tests/HLint.hs
@@ -1,9 +1,9 @@
 module Main (main) where
 
-import           Control.Monad           (unless)
-import           Language.Haskell.HLint3 (hlint)
-import           System.Environment      (getArgs)
-import           System.Exit             (exitFailure)
+import           Control.Monad          (unless)
+import           Language.Haskell.HLint (hlint)
+import           System.Environment     (getArgs)
+import           System.Exit            (exitFailure)
 
 
 main :: IO ()

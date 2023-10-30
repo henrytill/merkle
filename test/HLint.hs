ad6689fb0b00267949fd8d6545d114bda75dc6d1
Henry Till <henrytill@gmail.com>
2023-10-29 21:58:44 -0700

    move tests -> test

--- test/HLint.hs
+++ test/HLint.hs
@@ -8,5 +8,5 @@ import System.Exit (exitFailure)
 main :: IO ()
 main = do
   args <- getArgs
-  hints <- hlint (["src", "tests"] ++ args)
+  hints <- hlint (["src", "test"] ++ args)
   unless (null hints) exitFailure

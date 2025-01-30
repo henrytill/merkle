3506246ae581db18e451bd7a52518f3367abdce6
Henry Till <henrytill@gmail.com>
2025-01-29 17:55:44 -0800

    flake.nix: reformat

--- flake.nix
+++ flake.nix
@@ -10,25 +10,44 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    let makeMerkle = system: { compiler ? "ghc948" }:
-      let pkgs = nixpkgs.legacyPackages.${system};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    let
+      makeMerkle =
+        system:
+        {
+          compiler ? "ghc948",
+        }:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
           systemDepends = [ pkgs.lmdb ];
           call = compiler: pkgs.haskell.packages.${compiler}.callCabal2nixWithOptions;
           flags = "";
-          src = builtins.path { path = ./.; name = "merkle-src"; };
-          merkle_ = call compiler "merkle" src flags {};
-      in
+          src = builtins.path {
+            path = ./.;
+            name = "merkle-src";
+          };
+          merkle_ = call compiler "merkle" src flags { };
+        in
         pkgs.haskell.lib.overrideCabal merkle_ (oldAttrs: {
           executableSystemDepends = systemDepends;
           librarySystemDepends = systemDepends;
           testSystemDepends = systemDepends;
         });
     in
-      flake-utils.lib.eachDefaultSystem (system:
-        let merkle = makeMerkle system;
-        in {
-          packages.merkle = merkle {};
-          packages.default = self.packages.${system}.merkle;
-        });
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        merkle = makeMerkle system;
+      in
+      {
+        packages.merkle = merkle { };
+        packages.default = self.packages.${system}.merkle;
+      }
+    );
 }
